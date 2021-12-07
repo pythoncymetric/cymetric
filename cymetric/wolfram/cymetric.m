66edf02f33e515bc8a51738dbee432dc40f30a9e
ruehlef <f.ruehle@northeastern.edu>
2021-12-06 23:21:00 -0500

    Update cymetric.m

--- cymetric/wolfram/cymetric.m
+++ cymetric/wolfram/cymetric.m
@@ -116,7 +116,7 @@ pip=FileNameJoin[{path,"bin","pip"}];
 If[!FileExistsQ[pip],pip=FileNameJoin[{path,"bin","pip3"}];];
 If[!FileExistsQ[pip],Print["Error: Couldn't find pip at ",FileNameJoin[{path,"bin"}]];Return[python];];
 (*Install packages*);
-packages={ "h5py","joblib","numpy","pyyaml","pyzmq","scipy","sympy","tensorflow==2.4.1","wolframclient"};
+packages={ "h5py","joblib","numpy","pyyaml","pyzmq","scipy","sympy","tensorflow","wolframclient"};
 Print["Upgrading pip..."];
 installPackages=RunProcess[{pip,"install", "--upgrade", "pip"}];
 If[installPackages["ExitCode"]!=0,Print["An error occurred. Here's the output"];Print[installPackages["StandardOutput"]];Print[installPackages["StandardError"]];Return[python];];
