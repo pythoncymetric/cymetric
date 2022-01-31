2cfc11c7a924e113368995521d268f13547eea6a
ruehlef <f.ruehle@northeastern.edu>
2022-01-31 14:55:11 +0100

    Update cymetric.m

--- cymetric/wolfram/cymetric.m
+++ cymetric/wolfram/cymetric.m
@@ -521,7 +521,7 @@ If[Length[Dimensions[dataset]]==1,pts={dataset},pts=dataset];
 ];
 {omegaSquared,tmp}=GetOmegaSquared[dataset,"Python"->python,"Session"->session,"Dir"->outDir];
 {gs,tmp}=CYMetric[pts,"Python"->python,"Session"->session,"Model"->model,"Kappa"->kappa,"Dir"->outDir];
-dets=Table[Det[gs[[i,2]]],{i,Length[gs]}];
+dets=Table[Det[gs[[i]]],{i,Length[gs]}];
 dets=dets /volJNorm;
 res=omegaSquared/dets;
 Return[{Re[Normal[res]],session}];
